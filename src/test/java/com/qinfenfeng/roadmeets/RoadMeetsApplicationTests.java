74e4785188417fae89a25896496305c90778d109
TianZhou <761702168@QQ.COM>
2020-01-19 15:53:44 +0800

    删除多余的road-meets

---
+++ src/test/java/com/qinfenfeng/roadmeets/RoadMeetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qinfenfeng.roadmeets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadMeetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

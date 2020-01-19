90c251f4853ff926ab6cd9d00a0571dc303f3727
TianZhou <761702168@QQ.COM>
2020-01-19 16:27:09 +0800

    寻找丢失文件

--- src/main/java/com/qinfenfeng/roadmeets/RoadMeetsApplication.java
+++ src/main/java/com/qinfenfeng/roadmeets/RoadMeetsApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RoadMeetsApplication {
 
-
-    
     public static void main(String[] args) {
         SpringApplication.run(RoadMeetsApplication.class, args);
     }
